cf50d8b70bd7fcb8234f20456ce29b7ede6b42de
AdarshVp77 <adarshvjayan77@gmail.com>
2024-05-03 16:01:30 +0530

    second commit

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -6,64 +6,53 @@ const Login = () => {
   return (
     <>
       <Navbar />
-        {/* Header Start */}
-        <div>
-          <h1
-            className="text-center py-2"
-            style={{ fontSize: "50px", fontFamily: "serif"}}
-          >
-            Login
-          </h1>
+      {/* Header Start */}
+      <div>
+        <div class="px-4 py-5 text-center title-header">
+          <h1 className="m-4 text-dark">Login</h1>
         </div>
-        {/* Header End */}
+      </div>
+      {/* Header End */}
 
-        {/* Login Start */}
-        <div className="container-xxl py-5">
-          <div className="container">
-            <div className="row g-4 justify-content-center">
-              <div className="col-lg-6 col-md-8">
-                <form>
-                  <div className="form-floating mb-4">
-                    <input
-                      type="email"
-                      className="form-control"
-                      id="email"
-                      placeholder="name@example.com"
-                    />
-                    <label htmlFor="email">Email Address</label>
-                  </div>
-                  <div className="form-floating mb-4">
-                    <input
-                      type="password"
-                      className="form-control"
-                      id="password"
-                      placeholder="Password"
-                    />
-                    <label htmlFor="password">Password</label>
-                  </div>
-                  <div class="form-check m-4">
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      value=""
-                      id="flexCheckDefault"
-                    />
-                    <label className="form-check-label" for="flexCheckDefault" style={{color: "#fff"}}>
-                      Remember me
-                    </label>
-                  </div>
-                  <button type="submit" className="login-btn btn btn-dark w-100 py-3">
-                    Log in
-                  </button>
-                </form>
-              </div>
+      {/* Login Start */}
+      <div className="container-xxl py-5">
+        <div className="container">
+          <div className="row g-4 justify-content-center">
+            <div className="col-lg-6 col-md-8">
+              <form>
+                <div className="form-floating mb-4">
+                  <input
+                    type="email"
+                    className="form-control mt-3"
+                    id="email"
+                    placeholder="name@example.com"
+                  />
+                  <label htmlFor="email">Email Address</label>
+                </div>
+                <div className="form-floating mb-4">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    placeholder="Password"
+                  />
+                  <label htmlFor="password">Password</label>
+                </div>
+                <button
+                  type="submit"
+                  className="login-btn btn btn-dark w-100 py-3"
+                >
+                  Log in
+                </button>
+              </form>
             </div>
           </div>
         </div>
+      </div>
 
-        {/* Login End */}
+      {/* Login End */}
 
-        <Footer />
+      <Footer />
     </>
   );
 };
